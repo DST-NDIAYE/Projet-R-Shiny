34e0413e81971f3c005696b5c8a04d0679ee154a
Serigne Bassirou Ndiaye <dstndiaye@gmail.com>
2024-12-04 21:56:09 +0100

    firt

---
+++ app.R
@@ -0,0 +1,50 @@
+
+
+library(shiny)
+
+ui <- fluidPage(
+   
+  fileInput("file1", "Choisissez un fichier"),
+
+ #Must be set within the UI
+tableOutput(outputId = "mytable"),
+
+
+
+  verbatimTextOutput(outputId = "metadata") 
+   
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+
+  data <- reactive( {
+
+    file = input$file1  
+     if (is.null(file)) return("Aucun fichier téléchargé")
+
+  read.csv( file$datapath , header = TRUE )
+  }
+  )
+
+  output$metadata <- renderPrint({
+    summary(data())
+  })
+
+
+
+ #Must be set within the server
+output$mytable <- renderTable({
+  data()
+
+})
+
+
+
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
