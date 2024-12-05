ffbfbc1e5187a58ac90abb9e349570141e361485
Serigne Bassirou Ndiaye <dstndiaye@gmail.com>
2024-12-05 13:45:44 +0100

    Construiction la table dans la variable réactive tabStats

--- app.R
+++ app.R
@@ -12,11 +12,12 @@ ui <- fluidPage(
     column(9 ,verbatimTextOutput(outputId = "metadata") )
 
   ),
+  hr(),
 
   fluidRow(
 
     column(2 , actionButton(inputId = "go" , label ="charger") ),
-    column(8 , tableOutput(outputId = "mytable"))
+    column(10 , tableOutput(outputId = "mytable"))
 
   )
 
@@ -28,27 +29,39 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-
-
-
   data <- eventReactive( input$go ,  {
-
     file = input$file1  
-     if (is.null(file)) return("Aucun fichier téléchargé")
+    if (is.null(file)) return("Aucun fichier téléchargé")
+    read.csv( file$datapath , header = TRUE )
+  })
 
-  read.csv( file$datapath , header = TRUE )
-  }
-  )
+  tabStats <- reactive({
+    # Calculer les effectifs et les effectifs cumulés
+    table.tmp <- as.data.frame(table(data()))
+    table.tmp <- cbind(table.tmp, cumsum(table.tmp[[2]]))
+    # Calculer les fréquences et les fréquences cumulés
+    table.tmp <- cbind(table.tmp, 
+                       table.tmp[[2]]/nrow(data())*100,
+                       table.tmp[[3]]/nrow(data())*100)
+    # Ajouter des noms de colonnes
+    colnames(table.tmp) <- c("Ages", "Effectifs", "Effectifs Cum.",
+                             "Fréquences", "Fréquences Cum.")
+    
+    table.tmp <- table.tmp[,c(1, 4, 2, 5, 3)]
+    # Renvoyer le tableau statistique
+    table.tmp
+  })
+
+
 
   output$metadata <- renderPrint({
     summary(data())
   })
 
 
-
  #Must be set within the server
 output$mytable <- renderTable({
-  data()
+  tabStats()
 
 })
 
