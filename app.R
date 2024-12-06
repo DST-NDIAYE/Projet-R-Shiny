f3b382f3b135943dd868dd596ecf48409919f3f0
Serigne Bassirou Ndiaye <dstndiaye@gmail.com>
2024-12-06 19:18:46 +0100

    configuration du menu

--- app.R
+++ app.R
@@ -1,129 +1,91 @@
 library(shiny)
+library(shinydashboard)
 
-# Contenu de l'interface
-ui <- fluidPage(
-  
-  fluidRow(
-    column(3, 
-           # Bouton de recherche du fichier à charger
-           fileInput(inputId = "file1", label = "Choose CSV File",
-                     accept = c("text/plain", ".csv")
-           ))
+ui <- dashboardPage(
+  # Barre d'en-tête avec un menu déroulant
+  dashboardHeader(
+    title = "Application Shiny",
+    dropdownMenu(
+      type = "notifications",  # Menu déroulant pour les notifications
+      notificationItem(
+        text = "3 nouvelles données chargées",
+        icon = icon("database"),
+        status = "info"
+      ),
+      notificationItem(
+        text = "1 erreur détectée",
+        icon = icon("exclamation-triangle"),
+        status = "danger"
+      )
+    ),
+    dropdownMenu(
+      type = "tasks",  # Menu déroulant pour les tâches
+      taskItem(
+        text = "Préparation des données",
+        value = 60,  # Barre de progression
+        color = "blue"
+      ),
+      taskItem(
+        text = "Modélisation",
+        value = 30,
+        color = "green"
+      )
+    )
   ),
-  fluidRow(
   
-    column(2, 
-           # Buton de chargement 'en retard'
-           actionButton(inputId = "go", label = "Load")),
-    column(2, align="center",
-           # Buton de mise à jour de la liste rv
-           actionButton(inputId = "frequences", label = "fréquences")),
-    column(2, align="center",
-           # Buton de mise à jour de la liste rv
-           actionButton(inputId = "effectifs", label = "effectifs"))
-
+  # Barre latérale pour naviguer entre les sections
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Accueil", tabName = "home", icon = icon("home")),
+      menuItem("Prétraitement", tabName = "preprocessing", icon = icon("sliders"),
+        menuSubItem("Choix des colonnes", tabName = "columns"),
+        menuSubItem("Normalisation", tabName = "normalization"),
+        menuSubItem("Gestion des classes", tabName = "class_balance")
+      ),
+      menuItem("Analyse exploratoire", tabName = "exploration", icon = icon("chart-bar")),
+      menuItem("Modélisation", tabName = "modeling", icon = icon("robot"))
+    )
   ),
-
-  fluidRow(
-    column(4,
-           # Zone d'affichage de l'histogramme
-           plotOutput(outputId = "hist"))
+  
+  # Corps principal où le contenu s'affichera
+  dashboardBody(
+    tabItems(
+      # Page d'accueil
+      tabItem(
+        tabName = "home",
+        h2("Bienvenue sur l'application Shiny !"),
+        p("Utilisez le menu pour naviguer entre les différentes sections.")
+      ),
+      
+      # Section Prétraitement
+      tabItem(
+        tabName = "columns",
+        h2("Choix des colonnes à utiliser")
+      ),
+      tabItem(
+        tabName = "normalization",
+        h2("Normalisation des données")
+      ),
+      tabItem(
+        tabName = "class_balance",
+        h2("Gestion des classes déséquilibrées")
+      ),
+      
+      # Section Analyse exploratoire
+      tabItem(
+        tabName = "exploration",
+        h2("Analyse exploratoire")
+      ),
+      
+      # Section Modélisation
+      tabItem(
+        tabName = "modeling",
+        h2("Modélisation des données")
+      )
+    )
   )
 )
 
-# Commandes à exécuter
-server <- function(input, output){
-  
-  data <- eventReactive(input$go, {
-    inFile <- input$file1
-    if (is.null(inFile)) return(NULL)
-    read.csv(inFile$datapath, header = TRUE)
-  })
-  
-  # Récupération des valeurs fecondite
-  fecondite <- reactive({
-    if(!"fecondite" %in% colnames(data())) return(NULL)
-    data()$fecondite
-  })
-  
-  # Histogrammes
-  # ----
-  output$effectifsHist <- renderPlot({
-    # Histogramme des effectifs
-    hist( fecondite(), freq = TRUE, cex.axis = 1.5, cex.main = 1.5,
-          main = "Histogramme de l'indice de fécondite", col = "blue",
-          xlab = "Indice de fécondité", ylab = "Effectifs", las = 1,
-          breaks = seq(0.8, 3, by = 0.2), right = FALSE, cex.lab = 1.5)
-  })
-  
-  output$effectifsHistFreqDens <- renderPlot({
-    # Histogramme des densités de fréquences
-    hist( fecondite(), freq = FALSE, cex.axis = 1.5, cex.main = 1.5,
-          main = "Histogramme de l'indice de fécondite", col = "green",
-          xlab = "Indice de fécondité", ylab = "Densité de fréquences", las = 1,
-          breaks = seq(0.8, 3, by = 0.2), right = FALSE, cex.lab = 1.5)
-  })
-  
-  
-  # Courbe cumulative
-  # ----
-  output$effectifsCumCurve <- renderPlot({
-    # Récupération des infos à partir de l'histogramme
-    tmp.hist <- hist( fecondite(), plot = FALSE,
-                      breaks = seq(0.8, 3, by = 0.2),
-                      right = FALSE)
-    # Courbe cumulative (effectifs)
-    plot(x = tmp.hist$breaks[-1], y = cumsum(tmp.hist$counts),
-         xlab = "Fécondité (borne sup. de chaque classe)",
-         ylab = "Effectifs cumulés", cex.axis = 1.5, cex.lab = 1.5,
-         main = "Courbe cumulative de l'indice fécondité",
-         type = "o", col = "blue", lwd = 2, cex.main = 1.5)
-        
-  })
-  
-  output$freqCumCurve <- renderPlot({
-    # Récupération des infos à partir de l'histogramme
-    tmp.hist <- hist( fecondite(), plot = FALSE,
-                      breaks = seq(0.8, 3, by = 0.2),
-                      right = FALSE)
-    # Courbe cumulative (fréquences)
-    plot(x = tmp.hist$breaks[-1], y = cumsum(tmp.hist$density*rep(0.2, 11)),
-         xlab = "Fécondité (borne sup. de chaque classe)",
-         ylab = "Fréquences cumulées", cex.axis = 1.5, cex.lab = 1.5,
-         main = "Courbe cumulative de l'indice fécondité",
-         type = "o", col = "green", lwd = 2, cex.main = 1.5)
-    
-  })
+server <- function(input, output) {}
 
-
-
-
-rv <- reactiveValues(hist_isFreq = TRUE, 
-                       hist_yLabel = "Effectifs", 
-                       hist_col = "blue")
-  # On observe les clicks
-  observeEvent(input$effectifs,{
-    rv$hist_isFreq <- TRUE; 
-    rv$hist_yLabel <- "Effectifs"; 
-    rv$hist_col <- "blue";
-  })
-  observeEvent(input$frequences,{
-    rv$hist_isFreq <- FALSE; 
-    rv$hist_yLabel <- "Densité de fréquences";
-    rv$hist_col <- "green"
-  })
-  
-  # Histogramme
-  # ----
-  output$hist <- renderPlot({
-    hist(fecondite(), freq = rv$hist_isFreq, cex.axis = 1.5, cex.main = 1.5,
-          main = "Histogramme de l'indice de fécondite", col = rv$hist_col,
-          xlab = "Indice de fécondité", ylab = rv$hist_yLabel, las = 1,
-          breaks = seq(0.8, 3, by = 0.2), right = FALSE, cex.lab = 1.5)
-  })
-
-
-  
-}
-# Association interface & commandes
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
